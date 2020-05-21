2552cd5792e6e511bf51a7b04c8956cfe5ca1f3c
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2020-05-21 00:51:18 -0500

    Remove unused modules

--- src/window.py
+++ src/window.py
@@ -15,11 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import codecs
-import locale
 import logging
 import os
-import urllib
 
 from gettext import gettext as _
 from gi.repository import GLib, Gtk, Handy

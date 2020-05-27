c627f507674b34957eb216957367240d406d3917
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2020-05-26 20:43:54 -0500

    Implement custom subset option

--- src/generator.py
+++ src/generator.py
@@ -76,13 +76,11 @@ class Generator(object):
         self._append_log(_('Generating fonts for %s:' % data['name']), bold=True)
 
         if self.ranges:
-            for range in self.ranges:
+            for range, unicodes in self.ranges.items():
                 font = TTFont(filename)
                 subs = Subsetter()
 
-                unicode_range =  self.__get_range(range)
-
-                subs.populate(unicodes=parse_unicodes(unicode_range))
+                subs.populate(unicodes=parse_unicodes(unicodes))
                 subs.subset(font)
                 self._write_font(font, data, range=range)
                 font.close()
@@ -124,7 +122,7 @@ class Generator(object):
                 self._append_log(_('Generated <i>%s</i> with <i>%s</i> glyphs.' % (filenameout, count)))
 
             if range:
-                css['unicode-range'] = self.__get_range(range)
+                css['unicode-range'] = self.ranges[range]
             if self.font_display > 0:
                 css['font-display'] = self.__get_font_display(self.font_display)
             self.css[slug][name] = css
@@ -170,6 +168,9 @@ class Generator(object):
 
             self._append_log(_('Generated <i>%s</i>.' % filename))
 
+        if len(css_sheets) == 0:
+            self._append_log(_('There is no CSS to generate.'), italic=True)
+
         return css_sheets
 
 
@@ -209,23 +210,6 @@ class Generator(object):
 
         return '\n'.join(properties)
         #return '; '.join(['{}: {}'.format(k, v) for k, v in style_dict.items()])
-
-    def __get_range(self, range):
-        ranges = {
-            'latin':        'U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,' +
-                            'U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2212,U+2215',
-            'latin-ext':    'U+0100-024F,U+0259,U+1E00-1EFF,U+20A0-20CF,U+2C60-2C7F,' +
-                            'U+A720-A7FF',
-            'cyrillic':     'U+0400-045F,U+0490-0491,U+04B0-04B1,U+2116',
-            'cyrillic-ext': 'U+0460-052F,U+1C80-1C88,U+20B4,U+2DE0-2DFF,' +
-                            'U+A640-A69F,U+FE2E-FE2F',
-            'greek':        'U+0370-03FF',
-            'greek-ext':    'U+1F00-1FFF',
-            'vietnamese':   'U+0102-0103,U+0110-0111,U+1EA0-1EF9,U+20AB',
-            'devanagari':   'U+0900-097F,U+1CD0-1CF6,U+1CF8-1CF9,U+200B-200D,' +
-                            'U+20A8,U+20B9,U+25CC,U+A830-A839,U+A8E0-A8FB'
-        }
-        return ranges[range]
 
     def __get_font_display(self, fd):
         fds = ['auto', 'block', 'swap', 'fallback', 'optional']
--- src/options.py
+++ src/options.py
@@ -17,6 +17,7 @@
 
 from gettext import gettext as _
 from gi.repository import GLib, GObject, Gio, Gtk, Handy
+from fontTools.subset import parse_unicodes
 
 
 @Gtk.Template(resource_path='/com/rafaelmardojai/WebfontKitGenerator/ui/options.ui')
@@ -39,6 +40,7 @@ class Options(Gtk.Box):
     greek_ext    = Gtk.Template.Child()
     vietnamese   = Gtk.Template.Child()
     devanagari   = Gtk.Template.Child()
+    custom       = Gtk.Template.Child()
 
     # CSS
     font_display = Gtk.Template.Child()
@@ -108,18 +110,38 @@ class Options(Gtk.Box):
         return formats
 
     def get_subsetting(self):
-        list = []
+        dict = {}
+        ranges = {
+            'latin':        'U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,' +
+                            'U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2212,U+2215',
+            'latin-ext':    'U+0100-024F,U+0259,U+1E00-1EFF,U+20A0-20CF,U+2C60-2C7F,' +
+                            'U+A720-A7FF',
+            'cyrillic':     'U+0400-045F,U+0490-0491,U+04B0-04B1,U+2116',
+            'cyrillic-ext': 'U+0460-052F,U+1C80-1C88,U+20B4,U+2DE0-2DFF,' +
+                            'U+A640-A69F,U+FE2E-FE2F',
+            'greek':        'U+0370-03FF',
+            'greek-ext':    'U+1F00-1FFF',
+            'vietnamese':   'U+0102-0103,U+0110-0111,U+1EA0-1EF9,U+20AB',
+            'devanagari':   'U+0900-097F,U+1CD0-1CF6,U+1CF8-1CF9,U+200B-200D,' +
+                            'U+20A8,U+20B9,U+25CC,U+A830-A839,U+A8E0-A8FB'
+        }
 
         if not self.subsetting.get_enable_expansion():
             return None
 
         for (button, name) in self.subset_btns:
             if button.get_active():
-                list.append(name)
+                dict[name] = ranges[name]
 
-        if len(list) == 0:
+        try:
+            if parse_unicodes(self.custom.get_text()):
+                dict['custom'] = self.custom.get_text()
+        except ValueError:
+            pass
+
+        if len(dict) == 0:
             return None
-        return list
+        return dict
 
     def get_font_display(self):
         return self.font_display.get_selected_index()

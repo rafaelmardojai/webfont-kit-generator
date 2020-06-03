5c8814297497e581f04abf9c056f56050926bb74
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2020-06-03 13:25:45 -0500

    FontWidget: code clean up

--- src/font.py
+++ src/font.py
@@ -33,10 +33,12 @@ class FontWidget(Handy.ActionRow):
         super().__init__(**kwargs)
 
         self.model = model
-        self.set_activatable(False)
 
+        self.set_activatable(False)
         self.set_title(font.data['name'])
-        subtitle = font.data['family'] + ' / ' + font.data['style'] + ' / ' + font.data['weight']
+        subtitle = ' / '.join((
+            font.data['family'], font.data['style'], font.data['weight']
+        ))
         self.set_subtitle(subtitle)
 
         icon = Gtk.Image.new_from_icon_name('edit-delete-symbolic', Gtk.IconSize.MENU)
@@ -44,8 +46,9 @@ class FontWidget(Handy.ActionRow):
         btn_remove.add(icon)
         btn_remove.get_style_context().add_class('destructive-action')
         btn_remove.get_style_context().add_class('flat')
-        self.add(btn_remove)
         btn_remove.connect('clicked', self.remove_font)
+        self.add(btn_remove)
+
         self.show_all()
 
     def remove_font(self, widget):

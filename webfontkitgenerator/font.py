f48c4894f65b92bff21bc9ebc60002f5484084c1
Rafael Mardojai CM <email@rafaelmardojai.com>
2023-05-08 20:05:13 -0500

    font: Code improvements

--- webfontkitgenerator/font.py
+++ webfontkitgenerator/font.py
@@ -5,11 +5,13 @@ from gi.repository import Adw, GObject, GLib, Gtk
 
 
 class Font(GObject.Object):
-    __gtype_name__ = "Font"
+    """Object representing a font"""
 
-    path = GObject.Property(type=str)
+    __gtype_name__ = 'Font'
 
-    def __init__(self, path, data):
+    path: str = GObject.Property(type=str)
+
+    def __init__(self, path: str, data: dict):
         super().__init__()
 
         self.path = path
@@ -17,19 +19,18 @@ class Font(GObject.Object):
 
 
 class FontRow(Adw.ActionRow):
-    __gtype_name__ = "FontRow"
+    __gtype_name__ = 'FontRow'
 
-    def __init__(self, font_data):
+    def __init__(self, font_data: dict):
         super().__init__()
 
-        self.set_title(font_data['name'])
-        subtitle = ' / '.join(
+        self.props.title = font_data['name']
+        self.props.subtitle = ' / '.join(
             (font_data['family'], font_data['style'], font_data['weight'])
         )
-        self.set_subtitle(subtitle)
 
         btn_remove = Gtk.Button(valign=Gtk.Align.CENTER)
-        btn_remove.set_icon_name('webfontkitgenerator-remove-symbolic')
+        btn_remove.props.icon_name = 'webfontkitgenerator-remove-symbolic'
         btn_remove.add_css_class('flat')
         btn_remove.add_css_class('circular')
         btn_remove.connect('clicked', self.remove_font)
